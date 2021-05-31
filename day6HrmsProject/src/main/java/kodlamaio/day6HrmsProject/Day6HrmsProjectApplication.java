7c1ae72e6ec4f3b475f4dc94469fddb28723615d
_Sumeyra <sumeyra.cak@gmail.com>
2021-05-31 19:05:00 +0300

    added swagger

--- day6HrmsProject/src/main/java/kodlamaio/day6HrmsProject/Day6HrmsProjectApplication.java
+++ day6HrmsProject/src/main/java/kodlamaio/day6HrmsProject/Day6HrmsProjectApplication.java
@@ -2,8 +2,15 @@ package kodlamaio.day6HrmsProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Day6HrmsProjectApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +21,14 @@ public class Day6HrmsProjectApplication {
 		 *  
 		 * */
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.day6HrmsProject"))                                      
+          .build();                                           
+    }
+	
 
 }
